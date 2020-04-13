a0b5b3bf9de8c5d1d1f70668bbafe9e0e937cdb0
Miroslav Bodecek <mbodecek@gmail.com>
2020-04-13 14:27:41 +0200

    add 907464, 907471

--- test/compatibility.spec.ts
+++ test/compatibility.spec.ts
@@ -1,6 +1,6 @@
-import { testBlock, contractField, delegateField } from "./queries";
+import { testBlock, contractField, delegateField } from './queries';
 
-describe("GraphQL server compatibility layer", () => {
+describe('GraphQL server compatibility layer', () => {
     beforeEach(() => {
         // 60s timeout for each test
         jasmine.DEFAULT_TIMEOUT_INTERVAL = 60 * 1000;
@@ -10,32 +10,34 @@ describe("GraphQL server compatibility layer", () => {
         it(`returns OK for ${block} (${description})`, async () => testBlock(block, additionalBlockFields));
     }
 
-    describe("on recent data", () => {
-        itReturnsOkForBlock("head", "latest protocol", [contractField, delegateField]);
-        itReturnsOkForBlock("896621", "006", [contractField, delegateField]);
+    describe('on recent data', () => {
+        itReturnsOkForBlock('head', 'latest protocol', [contractField, delegateField]);
+        itReturnsOkForBlock('896621', '006', [contractField, delegateField]);
     });
 
-    describe("on starting blocks", () => {
-        itReturnsOkForBlock("851969", "006");
-        itReturnsOkForBlock("655361", "005");
-        itReturnsOkForBlock("458753", "004");
-        itReturnsOkForBlock("204762", "003");
-        itReturnsOkForBlock("28083", "002");
-        itReturnsOkForBlock("2", "001");
+    describe('on starting blocks', () => {
+        itReturnsOkForBlock('851969', '006');
+        itReturnsOkForBlock('655361', '005');
+        itReturnsOkForBlock('458753', '004');
+        itReturnsOkForBlock('204762', '003');
+        itReturnsOkForBlock('28083', '002');
+        itReturnsOkForBlock('2', '001');
     });
 
-    xdescribe("on starting blocks", () => {
-        itReturnsOkForBlock("1", "000");
-        itReturnsOkForBlock("0", "Genesis");
+    xdescribe('on starting blocks', () => {
+        itReturnsOkForBlock('1', '000');
+        itReturnsOkForBlock('0', 'Genesis');
     });
 
-    describe("on operation sample", () => {
-        itReturnsOkForBlock("896065", "006", [contractField, delegateField]);
-        itReturnsOkForBlock("696617", "005", [contractField, delegateField]);
-        itReturnsOkForBlock("554813", "004", [contractField, delegateField]);
-        itReturnsOkForBlock("32959", "002", [contractField]);
-        itReturnsOkForBlock("3425", "001", [contractField]);
-        itReturnsOkForBlock("446", "001");
-        itReturnsOkForBlock("379", "001");
+    describe('on operation sample', () => {
+        itReturnsOkForBlock('907464', '006');
+        itReturnsOkForBlock('907471', '006');
+        itReturnsOkForBlock('896065', '006', [contractField, delegateField]);
+        itReturnsOkForBlock('696617', '005', [contractField, delegateField]);
+        itReturnsOkForBlock('554813', '004', [contractField, delegateField]);
+        itReturnsOkForBlock('32959', '002', [contractField]);
+        itReturnsOkForBlock('3425', '001', [contractField]);
+        itReturnsOkForBlock('446', '001');
+        itReturnsOkForBlock('379', '001');
     });
 });
